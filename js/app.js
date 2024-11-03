61793ccf5196d8584ca6140422b5a7feaf8924f5
lucifer <sako.delovoi@gmail.com>
2024-11-03 22:51:19 +0600

    Portfolio v1

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  effect: 'cards',
+  grabCursor: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
