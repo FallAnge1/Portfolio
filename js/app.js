b57d73e5dcb1d12cbdba116db48b81952b7df363
lucifer <sako.delovoi@gmail.com>
2024-11-04 01:57:24 +0600

    Portfolio v3

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ document.getElementById('email_me').addEventListener('click', (e) => {
   navigator.clipboard
     .writeText(textToCopy)
     .then(() => {
-      document.getElementById('copyed').classList.remove('hidden')
-      document.getElementById('email_me').textContent = 'sako.delovoi@gmail.com'
+      document.getElementById('email_me').textContent = 'Скопировано'
+      document.getElementById('copyed').textContent = 'sako.delovoi@gmail.com'
       setTimeout(() => {
-        document.getElementById('copyed').classList.add('hidden')
         document.getElementById('email_me').textContent = 'Email'
-      }, 3000)
+        document.getElementById('copyed').textContent = ''
+      }, 2000)
     })
     .catch((err) => {
       console.error('Ошибка при копировании текста: ', err)
