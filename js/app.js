659c2f6f17005b43922869e78e4932698c675b79
lucifer <sako.delovoi@gmail.com>
2024-11-05 01:24:26 +0600

    Portfolio v5

--- js/app.js
+++ js/app.js
@@ -16,3 +16,76 @@ document.getElementById('email_me').addEventListener('click', (e) => {
       console.error('Ошибка при копировании текста: ', err)
     })
 })
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+  ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true,
+  })
+
+  gsap.fromTo(
+    '.gsap-skills',
+    { opacity: 0 },
+    {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.gsap-skills',
+        scrub: true,
+      },
+    }
+  )
+  gsap.fromTo(
+    '.gsap-photo',
+    { x: -250, opacity: 0 },
+    {
+      x: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.gsap-photo',
+        scrub: true,
+      },
+    }
+  )
+  gsap.fromTo(
+    '.skills_left',
+    { x: -50, opacity: 0 },
+    {
+      x: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.skills_left',
+        scrub: true,
+      },
+    }
+  )
+  gsap.fromTo(
+    '.skills_right',
+    { x: +250, opacity: 0 },
+    {
+      x: 0,
+      opacity: 1,
+      scrollTrigger: {
+        trigger: '.skills_right',
+        scrub: true,
+      },
+    }
+  )
+  gsap.fromTo(
+    '.form',
+    { y: 0, opacity: 0 },
+    {
+      y: -100,
+      opacity: 1,
+      scrollTrigger: {
+        start: '-1000',
+        end: '-300',
+        trigger: '.form',
+        scrub: true,
+      },
+    }
+  )
+}
